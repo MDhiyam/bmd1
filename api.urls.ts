177290144c20284c24b0d311885f09dcb5f41b40
MDhiyam <151819833+MDhiyam@users.noreply.github.com>
2023-12-28 17:46:00 +0530

    Add files via upload
    Update on Services

---
+++ api.urls.ts
@@ -0,0 +1,3 @@
+export const apiUrls = {
+    authServiceApi : 'http://localhost:4200/api/auth'
+}
---
+++ app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass : "toast-top-right",
+      preventDuplicates : true,
+      timeOut : 3000,
+      easing : 'ease-in',
+      easeTime : 1000,
+    }),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
