aefbc0cd842b9b0722965112bda59cb404bd18de
MDhiyam <151819833+MDhiyam@users.noreply.github.com>
2023-12-28 17:44:04 +0530

    Update reset-password.component.ts

--- reset-password/reset-password.component.ts
+++ reset-password/reset-password.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { confirmPasswordValidator } from '../validators/confirm-password.validators';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../services/auth.service';
 
 
 @Component({
@@ -27,8 +30,12 @@ export class ResetPasswordComponent implements OnInit{
   }
 
   fb = inject(FormBuilder);
-
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  toastr = inject(ToastrService);
+  authService = inject(AuthService);
   resetForm !: FormGroup;
+  token !: string;
 
   ngOnInit(): void {
     this.resetForm = this.fb.group({
@@ -39,12 +46,28 @@ export class ResetPasswordComponent implements OnInit{
       validator : confirmPasswordValidator('password' , 'confirmPassword')
     }
     );
+    this.activatedRoute.params.subscribe(val=>{
+      this.token = val['token'];
+      console.log(this.token);
+    })
   }
 
   reset(){
-    console.log(this.resetForm.value);
-
-    //Kindly add toastr message as password changed successfully kindly login with your new password
+    let resetObj = {
+      token : this.token,
+      password : this.resetForm.value.password
+    }
+    this.authService.resetPasswordService(resetObj)
+    .subscribe({
+      next:(res)=>{
+        this.toastr.success('Password changed successfully kindly login with your new password!');
+        this.resetForm.reset();
+        this.router.navigate(['/']);
+      },
+      error:(err)=>{
+        console.log(err);
+      }
+    })
   }
 
 }
