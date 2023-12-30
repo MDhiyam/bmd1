09bea4953c794da6b39b78b80e40b7b61cc825c4
MDhiyam <151819833+MDhiyam@users.noreply.github.com>
2023-12-30 12:42:57 +0530

    Add files via upload
    Update on Guard files

---
+++ auth.guard.ts
@@ -0,0 +1,24 @@
+// auth.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if (this.authService.getIsAuthenticated()) {
+      return true;
+    } else {
+      // Redirect to login page if not authenticated
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+}
